608c833b56840b8cdca67c9773ddf7cc1cbada46
mxcat <mxcat.xxtremxx@gmail.com>
2017-04-19 09:32:33 -0500

    ejercicio de clase de n*3

---
+++ ALG_3EJER3/ALG_3EJER3/Module1.vb
@@ -0,0 +1,14 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim n As Integer
+        n = 2
+        Do While n < 100
+            n = n * 3
+            Console.WriteLine("el valor de n final es -----------> " + Str(n))
+        Loop
+        Console.ReadLine()
+
+    End Sub
+
+End Module
