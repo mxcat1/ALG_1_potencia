8def281cb7a1eb6f234ee53ae24a8a778ceb80d8
mxcat <mxcat.xxtremxx@gmail.com>
2017-04-19 08:51:14 -0500

    nuevocodigo

--- ALG_2DA/ALG_2DA/Module1.vb
+++ ALG_2DA/ALG_2DA/Module1.vb
@@ -1,6 +1,31 @@
 ﻿Module Module1
-
+    Dim r, s, t As Integer
     Sub Main()
+        Console.ForegroundColor = 10
+        Console.Write("ingrese un valor para la variable R: ")
+        r = Console.ReadLine()
+        Console.Write("ingrese un valor para la variable S: ")
+        s = Console.ReadLine()
+        Console.Write("ingrese un valor para la variable T: ")
+        t = Console.ReadLine()
+
+        Do Until r = 7
+            If t = 1 Then
+                s += 3
+                t = 0
+            Else
+                s += 5
+                t = 1
+
+            End If
+            r += 1
+
+        Loop
+
+        Console.WriteLine("EL VALOR FINAL DE R ES ------------> " & r)
+        Console.WriteLine("EL VALOR FINAL DE R ES ------------> " & s)
+        Console.WriteLine("EL VALOR FINAL DE R ES ------------> " & t)
+        Console.ReadLine()
 
     End Sub
 
