8afeef944ad2fa7e54b9cacf9b9523da52c85f7a
mxcat <mxcat.xxtremxx@gmail.com>
2017-04-19 09:49:56 -0500

    con do while y do until

--- ALG_3EJER3/ALG_3EJER3/Module1.vb
+++ ALG_3EJER3/ALG_3EJER3/Module1.vb
@@ -7,7 +7,18 @@
             n = n * 3
             Console.WriteLine("el valor de n final es -----------> " + Str(n))
         Loop
+        n = 2
+        Console.WriteLine("CON DO UNTIL")
+        Do Until Not n < 100
+            n = n * 3
+            Console.WriteLine("el valor de n final es -----------> " + Str(n))
+        Loop
+
+
+
         Console.ReadLine()
+
+
 
     End Sub
 
