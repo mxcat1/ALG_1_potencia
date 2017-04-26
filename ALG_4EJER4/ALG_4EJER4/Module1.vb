cc1ee6989f3f873185af23316fa8bd09f3ef40e6
usuario <usuario@labe06>
2017-04-26 09:15:50 -0500

    EJERCICIO 1 DEL CONTROL2

--- ALG_4EJER4/ALG_4EJER4/Module1.vb
+++ ALG_4EJER4/ALG_4EJER4/Module1.vb
@@ -19,8 +19,42 @@
 
         Console.WriteLine("el valor final de BB ---->" + Str(bb))
         Console.WriteLine("el valor final de FF ---->" + Str(ff))
-        Console.ReadLine()
+        Console.WriteLine("------------------------------------------------------------")
+        Console.WriteLine("CON DU UNTIL")
+        Console.Write("INGRESE EL VALOR PARA BB----------->")
+        bb = Console.ReadLine()
+        Console.Write("IGRESE EL VALOR PARA FF----------->")
+        ff = Console.ReadLine()
+        Do Until ff > 6
+            If ff > 3 Then
+                bb = bb + 4
+            End If
+            bb *= 2
+            ff += 1
 
+        Loop
+        bb -= 10
+
+        Console.WriteLine("el valor final de BB ---->" + Str(bb))
+        Console.WriteLine("el valor final de FF ---->" + Str(ff))
+        Console.WriteLine("--------------------------------------------")
+        Console.WriteLine("CON FOR")
+        Console.Write("INGRESE EL VALOR PARA BB----------->")
+        bb = Console.ReadLine()
+        Console.Write("IGRESE EL VALOR PARA FF----------->")
+        ff = Console.ReadLine()
+        For ff = ff To 6
+            If ff > 3 Then
+                bb = bb + 4
+            End If
+            bb *= 2
+
+        Next
+        bb -= 10
+
+        Console.WriteLine("el valor final de BB ---->" + Str(bb))
+        Console.WriteLine("el valor final de FF ---->" + Str(ff))
+        Console.ReadLine()
     End Sub
 
 End Module
