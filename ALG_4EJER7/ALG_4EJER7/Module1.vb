f7c6f4a3efeac7335d93495a67da91231a6a0f1e
usuario <usuario@labe05>
2017-05-03 08:56:49 -0500

    EJER 02-05-17

--- ALG_4EJER7/ALG_4EJER7/Module1.vb
+++ ALG_4EJER7/ALG_4EJER7/Module1.vb
@@ -1,7 +1,13 @@
 ﻿Module Module1
 
     Sub Main()
+        Dim ff, c As Integer
+        Console.Write("INGRESE UN NUEVO VALOR PARA FF-----------------------> ")
+        ff = Console.ReadLine()
+        c = 1
+        Do While c < 21
 
+        Loop
     End Sub
 
 End Module
