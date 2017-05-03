06c0a8512a2e2547a2dd0d3eadb71c94a77b2ece
usuario <usuario@labe05>
2017-05-03 08:53:42 -0500

    EJERCICIO 2 CLASE 02-05-17

--- ALG_4EJER6/ALG_4EJER6/Module1.vb
+++ ALG_4EJER6/ALG_4EJER6/Module1.vb
@@ -13,6 +13,30 @@
             c += 1
         Loop
         Console.WriteLine("EL NUEVO VALOR DE NN ES --------------> " + Str(NN))
+        Console.WriteLine("----------------------------------------------------------------")
+        Console.WriteLine("CON DO WHILE")
+        Console.Write("INGRESE UN VALOR PARA NN ------------>")
+        NN = Console.ReadLine()
+        c = 1
+        Do While c < 11
+            NN += 4
+            If c > 5 Then
+                NN -= 2
+            End If
+            c += 1
+        Loop
+        Console.WriteLine("EL NUEVO VALOR DE NN ES --------------> " + Str(NN))
+        Console.WriteLine("----------------------------------------------------------------")
+        Console.WriteLine("CON DO FOR")
+        Console.Write("INGRESE UN VALOR PARA NN ------------>")
+        NN = Console.ReadLine()
+        For x = 1 To 10
+            NN += 4
+            If x > 5 Then
+                NN -= 2
+            End If
+        Next
+        Console.WriteLine("EL NUEVO VALOR DE NN ES --------------> " + Str(NN))
         Console.ReadLine()
 
 
