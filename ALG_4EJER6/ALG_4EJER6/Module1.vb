3716a2b3fefb0df302a9bb297d0021388eab6648
usuario <usuario@labe05>
2017-05-03 08:48:08 -0500

    ejercicio 2 de clase 3-05-17

--- ALG_4EJER6/ALG_4EJER6/Module1.vb
+++ ALG_4EJER6/ALG_4EJER6/Module1.vb
@@ -1,6 +1,20 @@
 ﻿Module Module1
 
     Sub Main()
+        Dim NN, c As Integer
+        Console.Write("INGRESE UN VALOR PARA NN ------------>")
+        NN = Console.ReadLine()
+        c = 1
+        Do Until c = 11
+            NN += 4
+            If c > 5 Then
+                NN -= 2
+            End If
+            c += 1
+        Loop
+        Console.WriteLine("EL NUEVO VALOR DE NN ES --------------> " + Str(NN))
+        Console.ReadLine()
+
 
     End Sub
 
