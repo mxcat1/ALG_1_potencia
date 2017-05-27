5e4db26971058acdaee0ae8517dacc339314b679
CHRISTIAN NICOLL VILCA APAZA <c.vilca.apaza@isur.edu.pe>
2017-05-26 22:06:47 -0500

    TAREA EJER 1

---
+++ ALG_VARINDEXTAREA1/ALG_VARINDEXTAREA1/Module1.vb
@@ -0,0 +1,21 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim dd, mm, aa, uldias(12) As Integer
+        Dim meses(12) As String
+        uldias(1) = 31
+        uldias(2) = 28
+        uldias(3) = 31
+        uldias(4) = 30
+        uldias(5) = 31
+        uldias(6) = 30
+        uldias(7) = 31
+        uldias(8) = 31
+        uldias(9) = 30
+        uldias(10) = 31
+        uldias(11) = 30
+        uldias(12) = 31
+
+    End Sub
+
+End Module
