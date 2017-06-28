a9fb54a50935a48f83430ea4c147132de15c1a3d
usuario <usuario@labe05>
2017-06-28 09:40:21 -0500

    hoy

--- ALG_FUNCIONESEJER2/ALG_FUNCIONESEJER2/Module2.vb
+++ ALG_FUNCIONESEJER2/ALG_FUNCIONESEJER2/Module2.vb
@@ -9,34 +9,8 @@
             Console.WriteLine("ERROR - INGRESE UN VALOR DE 9 DIGITOS")
             n = Console.ReadLine()
         Loop
-        Dim g1, g2, g3 As Integer
-        Dim nnum As String = ""
-        If n = 0 Then
-            nnum = "CERO"
-        Else
-            g1 = Int(n / 1000000)
-            g2 = Int((n - (g1 * 1000000)) / 1000)
-            g3 = n - (g1 * 1000000) - (g2 * 1000)
-            If g1 = 1 Then
-                nnum = " UN MILLON "
-            Else
-                If g1 > 1 Then
-                    nnum = tredigitos(g1) + " MILLONES "
-                End If
-            End If
-            If g2 = 1 Then
-                nnum = nnum + " UN MIL "
-            Else
-                If g2 > 1 Then
-                    nnum = nnum + tredigitos(g2) + " MIL "
-                End If
-            End If
-            If g3 > 0 Then
-                nnum = nnum + tredigitos(g3)
-            End If
-        End If
-        Process.Start(Environment.GetFolderPath(Environment.SpecialFolder.System) & "\mspaint.exe", "D:\Imagen.jpg")
-        Console.WriteLine(nnum)
+        Process.Start("D:\Imagen.jpg")
+        Console.WriteLine(nueve_digitos(n))
         Console.ReadLine()
 
     End Sub
--- ALG_FUNCIONESEJER2FRM/ALG_FUNCIONESEJER2FRM/Form1.vb
+++ ALG_FUNCIONESEJER2FRM/ALG_FUNCIONESEJER2FRM/Form1.vb
@@ -1,6 +1,9 @@
 ﻿Public Class Form1
     Private Sub btnmostrar_Click(sender As Object, e As EventArgs) Handles btnmostrar.Click
         lblnomnum.Text = nuevedigitos(nudnum.Value)
+        For x = 1 To 5
+            Process.Start("D:\Imagen.jpg")
+        Next
     End Sub
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
