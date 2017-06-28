631d2ac52d6a1c9ee7bedc46a1effde33e5f2c22
CHRISTIAN NICOLL VILCA APAZA <c.vilca.apaza@isur.edu.pe>
2017-06-27 21:46:38 -0500

    sxx

--- ALG_FUNCIONESEJER1/ALG_FUNCIONESEJER1/Module1.vb
+++ ALG_FUNCIONESEJER1/ALG_FUNCIONESEJER1/Module1.vb
@@ -3,8 +3,8 @@
         Dim n As Integer
         Console.WriteLine("INGRESE UN VALOR DE 3 DIGITOS")
         n = Console.ReadLine()
-        Do While n < 0 Or n > 1000
-            Console.WriteLine("ERROR - INGRESE UN VALOR DE 3 DIGITOS")
+        Do While n < 0 Or n > 1000000000
+            Console.WriteLine("ERROR - INGRESE UN VALOR DE 9 DIGITOS")
             n = Console.ReadLine()
         Loop
         Console.WriteLine(numeroaletras(n))
