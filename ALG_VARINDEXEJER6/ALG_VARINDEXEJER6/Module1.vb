5fad06de644d4a57b6d0a84280a858f80fc25658
usuario <usuario@labe05>
2017-07-05 09:59:28 -0500

    lucho 05-07-2017

--- ALG_VARINDEXEJER6/ALG_VARINDEXEJER6/Module1.vb
+++ ALG_VARINDEXEJER6/ALG_VARINDEXEJER6/Module1.vb
@@ -3,7 +3,6 @@
     Sub Main()
         Dim nombres(13), persona(13) As String
         Dim notas(13), mayor, lugar(13), v As Integer
-
         nombres(1) = "Andrea"
         nombres(2) = "Lucia"
         nombres(3) = "Miguel"
@@ -29,7 +28,7 @@
         notas(10) = 17
         notas(11) = 12
         notas(12) = 17
-        notas(13) = 12
+        notas(13) = 14
         mayor = notas(1)
         v = 1
         For x = 1 To 13
@@ -50,12 +49,38 @@
 
         Next
 
+        If v = 1 Then
+            Console.WriteLine("LA NOTA MAYOR ES" + Str(mayor) + " Y LA TIENE")
+        Else
+            Console.WriteLine("LA NOTA MAYOR ES" + Str(mayor) + " Y LA TIENEN")
+        End If
         For x = 1 To v
-            Console.WriteLine("LA PERSONA " + persona(x) + " EN LA POSICION" + Str(lugar(x)) + " TIENE LA NOTA IGUAL A " + Str(mayor))
-        Next
+            Console.WriteLine("LA PERSONA " + persona(x) + " EN LA POSICION" + Str(lugar(x)))
 
+        Next
+        'PRUEBA PARA QUE SE PUEDA AVERIGUAR EL TAMAÑO DE UNA VARIABLE INDEXADA
+        Console.WriteLine(Str(UBound(lugar)) + Str(Int(Rnd() * 10)))
+        Dim pp(Int(Rnd() * 100)), d, c As Integer
+        For d = 1 To UBound(pp)
+            pp(d) = Int(Rnd() * 100)
+        Next
+        Console.WriteLine(UBound(pp))
+        d = 1
+        Try
+            Do While d > 0
+                If pp(d) <> 0 Then
+                    c += 1
+                End If
+                d = d + 1
+            Loop
+
+        Catch ex As Exception
+            Console.WriteLine(c)
+        End Try
+        'FIN DE LA PRUEBA
         'ALGORITMO MEJORADO EN CLASE 05-07-2017
         Console.ReadLine()
+
     End Sub
 
 End Module
