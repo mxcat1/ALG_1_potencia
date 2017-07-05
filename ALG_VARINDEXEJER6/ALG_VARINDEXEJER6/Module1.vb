3176dcaf9eedfb18841b40323eb2abeadc7e0066
usuario <usuario@labe05>
2017-07-05 09:10:15 -0500

    ALGORITMO MEJORADO EN CLASE 05-07-2017

--- ALG_VARINDEXEJER6/ALG_VARINDEXEJER6/Module1.vb
+++ ALG_VARINDEXEJER6/ALG_VARINDEXEJER6/Module1.vb
@@ -1,8 +1,8 @@
 ﻿Module Module1
 
     Sub Main()
-        Dim nombres(13), persona(2) As String
-        Dim notas(13), mayor, max(2), lugar(2), v As Integer
+        Dim nombres(13), persona(13) As String
+        Dim notas(13), mayor, max(13), lugar(13), v As Integer
 
         nombres(1) = "Andrea"
         nombres(2) = "Lucia"
@@ -23,7 +23,7 @@
         notas(4) = 10
         notas(5) = 13
         notas(6) = 14
-        notas(7) = 9
+        notas(7) = 17
         notas(8) = 14
         notas(9) = 14
         notas(10) = 17
@@ -32,41 +32,32 @@
         notas(13) = 12
         mayor = notas(1)
         v = 1
-        For x = 1 To 12
-            If mayor < notas(x + 1) Then
-                mayor = notas(x + 1)
-            End If
-            If mayor = notas(x + 1) Then
-                lugar(v) = x + 1
-                persona(v) = nombres(lugar(v))
-                max(v) = mayor
-                'Console.WriteLine("LA PERSONA " + persona(v) + " EN EL LUGAR" + Str(lugar(v)) + " TIENE LA NOTA IGUAL A " + Str(mayor))
-                'Console.WriteLine(Str(v))
-                If v = 1 Then
-                    v = 2
-                Else
-                    v = 1
+        For x = 1 To 13
+            If notas(x) > mayor Then
+                v = 1
+                persona(v) = nombres(x)
+                lugar(v) = x
+                mayor = notas(x)
+            Else
+                If mayor = notas(x) Then
+                    v = v + 1
+                    lugar(v) = x
+                    persona(v) = nombres(lugar(v))
+                    max(v) = mayor
+                    'Console.WriteLine("LA PERSONA " + persona(v) + " EN EL LUGAR" + Str(lugar(v)) + " TIENE LA NOTA IGUAL A " + Str(mayor))
+                    'Console.WriteLine(Str(v))
+
+
                 End If
             End If
 
         Next
-        'el caso de que haya mas de 2 valores mayores iguales la variable indexada que las almacenaria
-        'se crearia dependiendo de eso
-        If max(1) = max(2) Then
-            For g = 1 To 2
-                Console.WriteLine("LA NOTA MAYOR ES DE" + Str(mayor) + " Y LA TIENE " + persona(g))
-            Next
-        Else
-            If max(1) > max(2) Then
-                Console.WriteLine("LA NOTA MAYOR ES DE" + Str(mayor) + " Y LA TIENE " + persona(1))
-            Else
-                If max(2) > max(1) Then
-                    Console.WriteLine("LA NOTA MAYOR ES DE" + Str(mayor) + " Y LA TIENE " + persona(2))
-                End If
-            End If
-            End If
 
+        For x = 1 To v
+            Console.WriteLine("LA PERSONA " + persona(x) + " EN LA POSICION" + Str(lugar(x)) + " TIENE LA NOTA IGUAL A " + Str(mayor))
+        Next
 
+        'ALGORITMO MEJORADO EN CLASE 05-07-2017
         Console.ReadLine()
     End Sub
 
